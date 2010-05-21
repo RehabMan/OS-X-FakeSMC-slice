6eda861aa95d5a8e31d84b14e0178cb3adefdc91
mozodojo <mozodojo@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-05-21 03:48:21 +0000

    Removed automatically generated "build" folder from repository.
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@8 958d8502-8caa-4642-8a4d-47b05a98fa7a

--- fakesmc/build/fakesmc.build/Release Tiger/fakesmc.build/DerivedSources/fakesmc_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char fakesmcVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:fakesmc  PROJECT:fakesmc-2.5" "\n"; const double fakesmcVersionNumber __attribute__ ((used)) = (double)2.5;
--- fakesmc/build/pcsmc.build/Release/pcsmc.build/DerivedSources/pcsmc_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char pcsmcVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:pcsmc  PROJECT:pcsmc-2.5" "\n"; const double pcsmcVersionNumber __attribute__ ((used)) = (double)2.5;
