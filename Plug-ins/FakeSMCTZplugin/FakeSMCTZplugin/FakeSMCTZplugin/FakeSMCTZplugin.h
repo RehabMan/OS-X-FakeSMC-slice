0990bf844031e64f4835b2430a90b7efa57eb4cf
Slice <Slice@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-05-22 16:20:40 +0000

    FakeSMC plugin to read Thermal Zone temperature.
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@27 958d8502-8caa-4642-8a4d-47b05a98fa7a

---
+++ Plug-ins/FakeSMCTZplugin/FakeSMCTZplugin/FakeSMCTZplugin/FakeSMCTZplugin.h
@@ -0,0 +1,25 @@
+#include <IOKit/IOService.h>
+#include "IOKit/acpi/IOACPIPlatformDevice.h"
+#include <IOKit/IOTimerEventSource.h>
+#include <fakesmc.h>
+
+class TZPlugin : public IOService
+{
+    OSDeclareDefaultStructors(TZPlugin)    
+private:
+	int TCount, FCount;
+	IOACPIPlatformDevice * TZDevice;
+protected:
+	//	void	Update(SMCData node);
+	IOWorkLoop *		TZWorkLoop;
+	IOTimerEventSource * TZPollTimer;
+		
+public:
+	virtual IOService*	probe(IOService *provider, SInt32 *score);
+    virtual bool		start(IOService *provider);
+	virtual bool		init(OSDictionary *properties=0);
+	virtual void		free(void);
+	virtual void		stop(IOService *provider);
+	IOReturn			poller( void );
+};
+
