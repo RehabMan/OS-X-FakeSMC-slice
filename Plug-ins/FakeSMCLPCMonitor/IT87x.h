ea23375f771a6a8ab2b3eed33ad7171bf217d967
usr-sse2 <usr-sse2@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-06-02 13:48:17 +0000

    Added ITE_NORTHBRIDGE_TEMPERATURE_OFFSET and ITE_HEATSINK_TEMPERATURE_OFFSET to plist. If no key in plist, sensor will be disabled. Values need to be podobrany for every motherboard.
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@112 958d8502-8caa-4642-8a4d-47b05a98fa7a

---
+++ Plug-ins/FakeSMCLPCMonitor/IT87x.h
@@ -0,0 +1,59 @@
+/*
+ *  IT87x.h
+ *  FakeSMCLPCMonitor
+ *
+ *  Created by Mozodojo on 29/05/10.
+ *  Copyright 2010 mozodojo. All rights reserved.
+ *
+ *  This code contains parts of original code from Open Hardware Monitor
+ *  Copyright 2010 Michael Möller. All rights reserved.
+ *
+ */
+
+#ifndef IT87X_H 
+#define IT87X_H
+
+#include "SuperIO.h"
+
+const UInt8 IT87_ENVIRONMENT_CONTROLLER_LDN = 0x04;
+
+// ITE
+const UInt8 ITE_VENDOR_ID = 0x90;
+
+// ITE Environment Controller
+const UInt8 ITE_ADDRESS_REGISTER_OFFSET = 0x05;
+const UInt8 ITE_DATA_REGISTER_OFFSET = 0x06;
+
+// ITE Environment Controller Registers    
+const UInt8 ITE_CONFIGURATION_REGISTER = 0x00;
+const UInt8 ITE_TEMPERATURE_BASE_REG = 0x29;
+const UInt8 ITE_VENDOR_ID_REGISTER = 0x58;
+const UInt8 ITE_FAN_TACHOMETER_16_BIT_ENABLE_REGISTER = 0x0c;
+const UInt8 ITE_FAN_TACHOMETER_REG[] = { 0x0d, 0x0e, 0x0f, 0x80, 0x82 };
+const UInt8 ITE_FAN_TACHOMETER_EXT_REG[] = { 0x18, 0x19, 0x1a, 0x81, 0x83 };
+const UInt8 ITE_VOLTAGE_BASE_REG = 0x20;
+
+const float ITE_VOLTAGE_GAIN[] = {1, 1, 1, (6.8f / 10 + 1), 1, 1, 1, 1, 1 };
+
+class IT87x : public SuperIO
+{
+private:
+	SInt8 ITE_HEATSINK_TEMPERATURE_OFFSET;
+	SInt8 ITE_NORTHBRIDGE_TEMPERATURE_OFFSET;
+protected:
+public:	
+	UInt8 	ReadByte(UInt8 reg, bool* valid);
+	UInt16	ReadRPM(UInt8 num);
+	void	Enter();
+	void	Exit();
+	
+	virtual void	SetTemperatureOffsets(UInt8 hsOffset, UInt8 nbOffset);
+	
+	virtual bool	Probe();
+	virtual void	Init();
+	virtual void	Finish();
+	
+	virtual void	Update(const char* key, char* data);
+};
+
+#endif
---
+++ Plug-ins/FakeSMCLPCMonitor/SuperIO.h
@@ -0,0 +1,94 @@
+/*
+ *  SuperIO.h
+ *  FakeSMCLPCMonitor
+ *
+ *  Created by Mozodojo on 29/05/10.
+ *  Copyright 2010 mozodojo. All rights reserved.
+ *
+ *  This code contains parts of original code from Open Hardware Monitor
+ *  Copyright 2010 Michael Möller. All rights reserved.
+ *
+ */
+
+#ifndef _SUPERIO_H 
+#define _SUPERIO_H
+
+#include <libkern/OSTypes.h>
+#include <architecture/i386/pio.h>
+#include <IOKit/IOLib.h>
+
+const UInt8 SUPERIO_REGISTER_PORT[2] = { 0x2e, 0x4e };
+const UInt8 SUPERIO_VALUE_PORT[2] = { 0x2f, 0x4f };
+
+// Registers
+const UInt8 SUPERIO_CONFIGURATION_CONTROL_REGISTER	= 0x02;
+const UInt8 SUPERIO_DEVCIE_SELECT_REGISTER			= 0x07;
+const UInt8 SUPERIO_CHIP_ID_REGISTER				= 0x20;
+const UInt8 SUPERIO_CHIP_REVISION_REGISTER			= 0x21;
+const UInt8 SUPERIO_BASE_ADDRESS_REGISTER			= 0x60;
+
+enum SuperIOModel
+{
+	UnknownModel = 0,
+	
+    IT8712F = 0x8712,
+    IT8716F = 0x8716,
+    IT8718F = 0x8718,
+    IT8720F = 0x8720,
+    IT8726F = 0x8726,
+	
+	W83627DHG = 0xA020,
+    W83627DHGP = 0xB070,
+    W83627EHF = 0x8800,    
+    W83627HF = 0x5200,
+	W83627THF = 0x8283,
+    W83667HG = 0xA510,
+    W83667HGB = 0xB350,
+    W83687THF = 0x8541,
+	
+    F71858 = 0x0507,
+    F71862 = 0x0601, 
+    F71869 = 0x0814,
+    F71882 = 0x0541,
+    F71889ED = 0x0909,
+    F71889F = 0x0723 
+};
+
+class SuperIO
+{
+private:
+protected:
+	UInt8	RegisterPort;
+	UInt8	ValuePort;
+	UInt16	Address;
+	
+	UInt16	LastVcore;
+	
+	UInt8	FanOffset;
+	UInt8	FanCount;
+	UInt8	FanIndex[5];
+
+	void	SetPorts(UInt8 index);
+	UInt8	ReadByte(UInt8 reg);
+	UInt16	ReadWord(UInt8 reg);
+	void	Select(UInt8 logicalDeviceNumber);
+public:
+	SuperIOModel	Model;
+	
+	virtual bool	Probe();
+	virtual void	Init();
+	virtual void	Finish();
+	
+	virtual void	Update(const char* key, char* data);
+	
+	const char*		GetModelNameString();
+};
+
+static SuperIO* Instance;
+
+static void Update(const char* key, char* data)
+{
+	Instance->Update(key, data);
+}
+
+#endif
