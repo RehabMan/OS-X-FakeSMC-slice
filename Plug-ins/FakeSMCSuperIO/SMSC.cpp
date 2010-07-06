74b0229774b8efb245d4b76f5ef43ef1d02d7ac3
Slice <Slice@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-07-06 19:44:41 +0000

    Datasheet SMSC for FAN control included
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@293 958d8502-8caa-4642-8a4d-47b05a98fa7a

--- Plug-ins/FakeSMCSuperIO/SMSC.cpp
+++ Plug-ins/FakeSMCSuperIO/SMSC.cpp
@@ -7,6 +7,55 @@
  *
  */
 
+//Slice - some comments from datasheets
+#if 0
+-----------------------------------------------------------
+switch (id) {
+	case 0x5a: /* LPC47N227 */
+		runtime_base = regval(port, 0x30) << 4;
+		if (runtime_base)
+			dump_io(runtime_base, 16);
+			else
+				printf("Runtime Register Block not mapped on this Super I/O.\n");
+				break;
+	default:
+		printf("No extra registers known for this chip.\n");
+}
+--------------------------------------------------------------
+SMSC LDNs
+0 Floppy
+1 PM1
+2 COM2 (47M182)
+3 LPT
+4 COM1
+5 COM2
+6 RTC
+7 Keyboard
+8 EC
+9 Mailbox 
+A LPC/8051 GPIO (FAN control), PME, Runtime Registers
+B MIDI port (MPU-401), SMBus
+----------------------------------------------------------------
+Fan Tachometer Register ( 1 =0x59, 2 =0x5A LDN=0x0A)
+Bit]7:0] The 8-bit FAN1 tachometer count. The number of counts of the internal clock per pulse of the fan. The count value is computed from Equation 1. This value is the final (maximum) count of the previous pulse (latched). The value in this register may not be valid for up to 2 pulses following a write to the preload register.
+----------------------------------------------------------------
+Fan Control Register (0x58 LDN=0x0A)
+Bit[0] Fan 1 Clock Source Select
+This bit and the Fan 1 Clock Multiplier bit is used with The Fan 1 Clock Select bit in the Fan 1 register (0x56) to determine the fan speed FOUT.	See Table 56 in “Fan Speed Control and Monitoring” section.
+Bit[1] Fan 2 Clock Source Select This bit and the Fan 2 Clock Multiplier bit is used with The Fan 2 Clock Select bit in the Fan 2 register (0x57) to determine the fan speed FOUT.	See Table 56 in “Fan Speed Control and Monitoring” section. Bit[2] Fan 1 Clock multiplier
+0=No multiplier used
+1=Double the fan speed selected by bit 0 of this register and bit 7 of the FAN1 register
+Bit[3] Fan 2 Clock multiplier 0=No multiplier used
+1=Double the fan speed selected by bit 1 of this register and bit 7 of the FAN2 register
+Bit[5:4] The FAN1 count divisor. Clock scalar for adjusting the tachometer count. Default = 2.
+00: divisor = 1 01: divisor = 2 10: divisor = 4 11: divisor = 8
+Bit[7:6] The FAN2 count divisor. Clock scalar for adjusting the tachometer count. Default = 2.
+00: divisor = 1 01: divisor = 2 10: divisor = 4 11: divisor = 8
+-----------------------------------------------------------------------------
+
+
+#endif  //datasheets
+
 #include "SMSC.h"
 
 void SMSC::Enter()
