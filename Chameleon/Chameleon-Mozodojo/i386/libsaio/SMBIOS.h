fff2380dd533c4f29e3b19e4b7c188207c923114
mozodojo <mozodojo@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-07-16 21:38:44 +0000

    Latest Chameleon trunk with Fermi support, Mem Detection working & SSDT override!
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@329 958d8502-8caa-4642-8a4d-47b05a98fa7a

---
+++ Chameleon/Chameleon-Mozodojo/i386/libsaio/SMBIOS.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 1998-2006 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+ * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ * 
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+/* This file is a stripped-down version of the one found in the AppleSMBIOS project.
+ * Changes:
+ * - Don't use pragma pack but instead use GCC's packed attribute
+ */
+
+#ifndef _LIBSAIO_SMBIOS_H
+#define _LIBSAIO_SMBIOS_H
+
+/*
+ * Based on System Management BIOS Reference Specification v2.5
+ */
+
+typedef uint8_t		SMBString;
+typedef uint8_t		SMBByte;
+typedef uint16_t	SMBWord;
+typedef uint32_t	SMBDWord;
+typedef uint64_t	SMBQWord;
+
+struct DMIHeader {
+	SMBByte			type;
+	SMBByte			length;
+	SMBWord			handle;
+} __attribute__((packed));
+
+struct DMIEntryPoint {
+	SMBByte			anchor[5];
+	SMBByte			checksum;
+	SMBWord			tableLength;
+	SMBDWord		tableAddress;
+	SMBWord			structureCount;
+	SMBByte			bcdRevision;
+} __attribute__((packed));
+
+struct SMBEntryPoint {
+	SMBByte			anchor[4];
+	SMBByte			checksum;
+	SMBByte			entryPointLength;
+	SMBByte			majorVersion;
+	SMBByte			minorVersion;
+	SMBWord			maxStructureSize;
+	SMBByte			entryPointRevision;
+	SMBByte			formattedArea[5];
+	struct DMIEntryPoint	dmi;
+} __attribute__((packed));
+
+struct DMIMemoryControllerInfo {/* 3.3.6 Memory Controller Information (Type 5) */
+	struct DMIHeader	dmiHeader;
+	SMBByte			errorDetectingMethod;
+	SMBByte			errorCorrectingCapability;
+	SMBByte			supportedInterleave;
+	SMBByte			currentInterleave;
+	SMBByte			maxMemoryModuleSize;
+	SMBWord			supportedSpeeds;
+	SMBWord			supportedMemoryTypes;
+	SMBByte			memoryModuleVoltage;
+	SMBByte			numberOfMemorySlots;
+} __attribute__((packed));
+
+struct DMIMemoryModuleInfo {	/* 3.3.7 Memory Module Information (Type 6) */
+	struct DMIHeader	dmiHeader;
+	SMBByte			socketDesignation;
+	SMBByte			bankConnections;
+	SMBByte			currentSpeed;
+	SMBWord			currentMemoryType;
+	SMBByte			installedSize;
+	SMBByte			enabledSize;
+	SMBByte			errorStatus;
+} __attribute__((packed));
+
+struct DMIPhysicalMemoryArray {	/* 3.3.17 Physical Memory Array (Type 16) */
+	struct DMIHeader	dmiHeader;
+	SMBByte			location;
+	SMBByte			use;
+	SMBByte			memoryCorrectionError;
+	SMBDWord		maximumCapacity;
+	SMBWord			memoryErrorInformationHandle;
+	SMBWord			numberOfMemoryDevices;
+} __attribute__((packed));
+
+struct DMIMemoryDevice {	/* 3.3.18 Memory Device (Type 17) */
+	struct DMIHeader	dmiHeader;
+	SMBWord			physicalMemoryArrayHandle;
+	SMBWord			memoryErrorInformationHandle;
+	SMBWord			totalWidth;
+	SMBWord			dataWidth;
+	SMBWord			size;
+	SMBByte			formFactor;
+	SMBByte			deviceSet;
+	SMBByte			deviceLocator;
+	SMBByte			bankLocator;
+	SMBByte			memoryType;
+	SMBWord			typeDetail;
+        SMBWord                 speed;
+} __attribute__((packed));
+
+#endif /* !_LIBSAIO_SMBIOS_H */
---
+++ Chameleon/Chameleon-Mozodojo/i386/libsaio/pci_root.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2009 netkas
+ */
+
+#include "libsaio.h"
+#include "boot.h"
+#include "bootstruct.h"
+
+#ifndef DEBUG_PCIROOT
+#define DEBUG_PCIROOT 1
+#endif
+
+#if DEBUG_PCIROOT
+#define DBG(x...)  printf(x)
+#else
+#define DBG(x...)
+#endif
+
+static int rootuid = 10; //value means function wasnt ran yet
+
+static unsigned int findrootuid(unsigned char * dsdt, int len)
+{
+	int i;
+	for (i=0; i<64 && i<len-5; i++) //not far than 64 symbols from pci root 
+	{
+		if(dsdt[i] == '_' && dsdt[i+1] == 'U' && dsdt[i+2] == 'I' && dsdt[i+3] == 'D' && dsdt[i+5] == 0x08)
+		{
+			return dsdt[i+4];
+		}
+	}
+	return 11;
+}
+
+static unsigned int findpciroot(unsigned char * dsdt,int len)
+{
+	int i;
+
+	for (i=0; i<len-4; i++) {
+		if(dsdt[i] == 'P' && dsdt[i+1] == 'C' && dsdt[i+2] == 'I' && (dsdt[i+3] == 0x08 || dsdt [i+4] == 0x08)) {
+			return findrootuid(dsdt+i, len-i);
+		}
+	}
+	return 10;
+}
+
+int getPciRootUID(void)
+{
+	void *new_dsdt;
+	const char *val;
+	int len,fsize;
+	const char * dsdt_filename=NULL;
+	extern int search_and_get_acpi_fd(const char *, const char **);
+
+	if (rootuid < 10) return rootuid;
+	rootuid = 0;	/* default uid = 0 */
+
+	if (getValueForKey(kPCIRootUID, &val, &len, &bootInfo->bootConfig)) {
+		if (isdigit(val[0])) rootuid = val[0] - '0';
+		goto out;
+	}
+	/* Chameleon compatibility */
+	else if (getValueForKey("PciRoot", &val, &len, &bootInfo->bootConfig)) {
+		if (isdigit(val[0])) rootuid = val[0] - '0';
+		goto out;
+	}
+	/* PCEFI compatibility */
+	else if (getValueForKey("-pci0", &val, &len, &bootInfo->bootConfig)) {
+		rootuid = 0;
+		goto out;
+	}
+	else if (getValueForKey("-pci1", &val, &len, &bootInfo->bootConfig)) {
+		rootuid = 1;
+		goto out;
+	}
+
+	int fd = search_and_get_acpi_fd("DSDT.aml", &dsdt_filename);
+
+	// Check booting partition
+	if (fd<0)
+	{	  
+	  verbose("No DSDT found, using 0 as uid value.\n");
+	  rootuid = 0;
+	  goto out;
+	}
+	
+	fsize = file_size(fd);
+
+	if ((new_dsdt = malloc(fsize)) == NULL) {
+		verbose("[ERROR] alloc DSDT memory failed\n");
+		close (fd);
+		goto out;
+	}
+	if (read (fd, new_dsdt, fsize) != fsize) {
+		verbose("[ERROR] read %s failed\n", dsdt_filename);
+		close (fd);
+		goto out;
+	}
+	close (fd);
+
+	rootuid = findpciroot(new_dsdt, fsize);
+	free(new_dsdt);
+
+	// make sure it really works: 
+	if (rootuid == 11) rootuid=0; //usually when _UID isnt present, it means uid is zero
+	else if (rootuid < 0 || rootuid > 9) 
+	{
+		printf("PciRoot uid value wasnt found, using 0, if you want it to be 1, use -PciRootUID flag");
+		rootuid = 0;
+	}
+out:
+	verbose("Using PCI-Root-UID value: %d\n", rootuid);
+	return rootuid;
+}
