dac54a2cd40fe5a11f97b5bfd8e9c4f1e0e37ca6
Slice <Slice@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-07-23 07:47:30 +0000

    Restore X3100
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@355 958d8502-8caa-4642-8a4d-47b05a98fa7a

---
+++ Plug-ins/FakeSMCx3100plugin/FakeSMCx3100plugin/FakeSMCx3100plugin.h
@@ -0,0 +1,50 @@
+#include <IOKit/IOService.h>
+#include <IOKit/pci/IOPCIDevice.h>
+#include <IOKit/IOTimerEventSource.h>
+#include "FakeSMCBinding.h"
+
+class x3100plugin : public IOService //IOPCIDevice
+{
+    OSDeclareDefaultStructors(x3100plugin)    
+private:
+	volatile		UInt8* mmio_base;
+	UInt16			device_id;
+	IOPCIDevice *	VCard;
+	IOMemoryMap *	mmio;
+	FakeSMCBinding* m_Binding;
+protected:
+	//	IOWorkLoop *		TZWorkLoop;
+	//	IOTimerEventSource * TZPollTimer;
+
+	
+public:
+	IOReturn Update(const char* key, char* data);
+//	virtual int			update(int keyN);
+	virtual IOService*	probe(IOService *provider, SInt32 *score);
+    virtual bool		start(IOService *provider);
+	virtual bool		init(OSDictionary *properties=0);
+	virtual void		free(void);
+	virtual void		stop(IOService *provider);
+	//	IOReturn			poller( void );
+};
+
+class Binding : public FakeSMCBinding 
+{
+private:
+	x3100plugin* m_Monitor;
+	
+public:
+	Binding(x3100plugin* monitor)
+	{
+		m_Monitor = monitor;
+	}
+	
+	virtual IOReturn OnKeyRead(const char* key, char* data)
+	{
+		return m_Monitor->Update(key, data);
+		
+	};
+	
+	virtual IOReturn OnKeyWrite(__unused const char* key, __unused char* data)
+	{return kIOReturnUnsupported;};
+};
