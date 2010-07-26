8e826647419553c99784bfeb6733c7574341a4c2
Slice <Slice@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-07-26 11:03:37 +0000

    class FakeSMCBinding
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@380 958d8502-8caa-4642-8a4d-47b05a98fa7a

--- Plug-ins/FakeSMCSuperIO/FakeSMC.h
+++ Plug-ins/FakeSMCSuperIO/FakeSMC.h
@@ -40,8 +40,14 @@
 class FakeSMCBinding 
 {
 public:
-	virtual IOReturn OnKeyRead(const char* key, char* data);
-	virtual IOReturn OnKeyWrite(const char* key, char* data);
+	virtual IOReturn OnKeyRead(__unused const char* key, __unused char* data)
+	{
+		return kIOReturnInvalid;
+	};
+	virtual IOReturn OnKeyWrite(__unused const char* key, __unused char* data)
+	{
+		return kIOReturnInvalid;
+	};
 };
 
 struct AppleSMCData;
