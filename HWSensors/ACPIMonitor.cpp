30cc5236d69be7264e7a73024d62a2f501539f6c
mozodojo <mozodojo@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2010-12-08 17:01:31 +0000

    Bugfixes in ACPIMonitor
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@437 958d8502-8caa-4642-8a4d-47b05a98fa7a

--- HWSensors/ACPIMonitor.cpp
+++ HWSensors/ACPIMonitor.cpp
@@ -23,9 +23,12 @@ OSDefineMetaClassAndStructors(ACPIMonitor, IOService)
 bool ACPIMonitor::addSensor(const char* method, const char* key, const char* type, unsigned char size)
 {
 	if (kIOReturnSuccess == fakeSMC->callPlatformFunction(kFakeSMCAddKeyHandler, false, (void *)key, (void *)type, (void *)size, (void *)this))
-		return sensors->setObject(OSString::withCString(key), OSString::withCString(method));
+		if (sensors->setObject(key, OSString::withCString(method))) {
+			InfoLog("%s registered", method);
+			return true;
+		}
 	
-	return 0;
+	return false;
 }
 
 bool ACPIMonitor::addTachometer(const char* method, const char* caption)
@@ -55,17 +58,19 @@ bool ACPIMonitor::addTachometer(const char* method, const char* caption)
 				if (kIOReturnSuccess != fakeSMC->callPlatformFunction(kFakeSMCAddKeyValue, false, (void *)KEY_FAN_NUMBER, (void *)TYPE_UI8, (void *)1, (void *)&length))
 					WarningLog("error adding FNum value");
 			}
-			else if (kIOReturnSuccess == result) {
-				length = 0;
-				
-				bcopy(data, &length, 1);
-				
-				length++;
-				
-				if (kIOReturnSuccess != fakeSMC->callPlatformFunction(kFakeSMCSetKeyValue, false, (void *)KEY_FAN_NUMBER, (void *)1, (void *)&length, 0))
-					WarningLog("error updating FNum value");
+			else {
+				if (kIOReturnSuccess == result) {
+					length = 0;
+					
+					bcopy(data, &length, 1);
+					
+					length++;
+					
+					if (kIOReturnSuccess != fakeSMC->callPlatformFunction(kFakeSMCSetKeyValue, false, (void *)KEY_FAN_NUMBER, (void *)1, (void *)&length, 0))
+						WarningLog("error updating FNum value");
+				}
+				else WarningLog("error reading FNum value");
 			}
-			else WarningLog("error reading FNum value");
 			
 			return true;
 		}
@@ -107,83 +112,57 @@ bool ACPIMonitor::start(IOService * provider)
 			if (fanNames )
 				name = OSDynamicCast(OSString, fanNames->getObject(i));
 			
-			if (addTachometer(key, name ? name->getCStringNoCopy() : 0)) {
-				WarningLog("Can't add tachometer sensor, kext will not load");
-				return false;
-			}
+			if (!addTachometer(key, name ? name->getCStringNoCopy() : 0))
+				WarningLog("Can't add tachometer sensor, key %s", key);
 		} 
-		else {
-			break;
-		}
+		else break;
 	}
 
 	//Next step - temperature keys
-	if (kIOReturnSuccess == acpiDevice->validateObject("TSN0")){		
-		addSensor("TSN0", "TC0H", TYPE_SP78, 2);
-		InfoLog("%s registered", "TC0H");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("TSN1")){		
-		addSensor("TSN1", "TN0H", TYPE_SP78, 2);
-		InfoLog("%s registered", "TN0H");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("TSN2")){		
-		addSensor("TSN2", "TW0P", TYPE_SP78, 2);
-		InfoLog("%s registered", "TW0P");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("TSN3")){		
-		addSensor("TSN3", "Th1H", TYPE_SP78, 2);
-		InfoLog("%s registered", KEY_CPU_HEATSINK_TEMPERATURE);
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("TSN4")){		
-		addSensor("TSN4", "Th1H", TYPE_SP78, 2);
-		InfoLog("%s registered", "Th1H");
-	}
+	if (kIOReturnSuccess == acpiDevice->validateObject("TCPH"))
+		addSensor("TCPH", KEY_CPU_HEATSINK_TEMPERATURE, TYPE_SP78, 2);
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("TSYS"))
+		addSensor("TSYS", KEY_NORTHBRIDGE_TEMPERATURE, TYPE_SP78, 2);
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("TDIM"))
+		addSensor("TDIM", KEY_DIMM_TEMPERATURE, TYPE_SP78, 2);
+		
+	if (kIOReturnSuccess == acpiDevice->validateObject("TAMB"))
+		addSensor("TAMB", KEY_AMBIENT_TEMPERATURE, TYPE_SP78, 2);
 	
 	//Voltage
-	if (kIOReturnSuccess == acpiDevice->validateObject("VSN0")){		
+	if (kIOReturnSuccess == acpiDevice->validateObject("VSN0"))
 		addSensor("VSN0", "VCAC", TYPE_FP2E, 2);
-		InfoLog("%s registered", "VCAC");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("VSN1")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("VSN1"))
 		addSensor("VSN1", "Vp0C", TYPE_FP2E, 2);
-		InfoLog("%s registered", "Vp0C");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("VSN2")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("VSN2"))
 		addSensor("VSN2", "Vp1C", TYPE_FP2E, 2);
-		InfoLog("%s registered", "Vp1C");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("VSN3")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("VSN3"))
 		addSensor("VSN3", "Vp2C", TYPE_FP2E, 2);
-		InfoLog("%s registered", "Vp2C");
-	}
 	
 	//Amperage
-	if (kIOReturnSuccess == acpiDevice->validateObject("ISN0")){		
+	if (kIOReturnSuccess == acpiDevice->validateObject("ISN0"))
 		addSensor("ISN0", "ICAC", TYPE_UI16, 2);
-		InfoLog("%s registered", "ICAC");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("ISN1")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("ISN1"))
 		addSensor("ISN1", "Ip0C", TYPE_UI16, 2);
-		InfoLog("%s registered", "Ip0C");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("ISN2")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("ISN2"))
 		addSensor("ISN2", "Ip1C", TYPE_UI16, 2);
-		InfoLog("%s registered", "Ip1C");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("ISN3")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("ISN3"))
 		addSensor("ISN3", "Ip2C", TYPE_UI16, 2);
-		InfoLog("%s registered", "Ip2C");
-	}
 	
 	//Power
-	if (kIOReturnSuccess == acpiDevice->validateObject("PSN0")){		
+	if (kIOReturnSuccess == acpiDevice->validateObject("PSN0"))
 		addSensor("PSN0", "PC0C", TYPE_UI16, 2);
-		InfoLog("%s registered", "PC0C");
-	}
-	if (kIOReturnSuccess == acpiDevice->validateObject("PSN1")){		
+
+	if (kIOReturnSuccess == acpiDevice->validateObject("PSN1"))
 		addSensor("PSN1", "PC1C", TYPE_UI16, 2);
-		InfoLog("%s registered", "PC1C");
-	}	
 	
 	registerService(0);
 
