a7af4ce9e792240e4febad9ddd3f2d18439611b9
slice <slice@958d8502-8caa-4642-8a4d-47b05a98fa7a>
2013-02-19 13:22:53 +0000

    add geforce sensors to project
    git-svn-id: https://subversion.assembla.com/svn/fakesmc@690 958d8502-8caa-4642-8a4d-47b05a98fa7a

--- HWSensors/NVClockX.h
+++ HWSensors/NVClockX.h
@@ -21,7 +21,7 @@ class NVClockX : public IOService
     OSDeclareDefaultStructors(NVClockX)    
 	
 private:
-	IOService *		fakeSMC;
+	IOService *     fakeSMC;
 	OSDictionary *	sensors;
 	
 	IOMemoryMap *	nvio;
@@ -31,12 +31,12 @@ private:
 	int				addTachometer(int index);
 	
 public:
-	virtual bool		init(OSDictionary *properties=0);
+	virtual bool        init(OSDictionary *properties=0);
 	virtual IOService*	probe(IOService *provider, SInt32 *score);
-    virtual bool		start(IOService *provider);
-	virtual void		stop(IOService *provider);
-	virtual void		free(void);
+  virtual bool        start(IOService *provider);
+	virtual void        stop(IOService *provider);
+	virtual void        free(void);
 	
-	virtual IOReturn	callPlatformFunction(const OSSymbol *functionName, bool waitForFunction, void *param1, void *param2, void *param3, void *param4 ); 
+	virtual IOReturn    callPlatformFunction(const OSSymbol *functionName, bool waitForFunction, void *param1, void *param2, void *param3, void *param4 ); 
 	
 };
--- HWSensors/X3100.h
+++ HWSensors/X3100.h
@@ -17,19 +17,19 @@ class X3100monitor : public IOService
 private:
 	IOService*			fakeSMC;
 	OSDictionary*		sensors;
-	volatile			UInt8* mmio_base;
-	int					numCard;  //numCard=0 if only one Video, but may be any other value
+	volatile        UInt8* mmio_base;
+	int             numCard;  //numCard=0 if only one Video, but may be any other value
 	IOPCIDevice *		VCard;
 	IOMemoryMap *		mmio;
 	
-	bool				addSensor(const char* key, const char* type, unsigned char size, int index);
+	bool            addSensor(const char* key, const char* type, unsigned char size, int index);
 	
 public:
 	virtual IOService*	probe(IOService *provider, SInt32 *score);
-    virtual bool		start(IOService *provider);
-	virtual bool		init(OSDictionary *properties=0);
-	virtual void		free(void);
-	virtual void		stop(IOService *provider);
+  virtual bool        start(IOService *provider);
+	virtual bool        init(OSDictionary *properties=0);
+	virtual void        free(void);
+	virtual void        stop(IOService *provider);
 	
 	virtual IOReturn	callPlatformFunction(const OSSymbol *functionName, bool waitForFunction, void *param1, void *param2, void *param3, void *param4 ); 
 };
